4d92b4e77272f0c80fab9b4c2036e5cb1cc1043f
FishInPool <huafen999@126.com>
2017-06-28 20:16:48 +0800

    Add files via upload

---
+++ yytianqi/jss.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	getWeather($("#city").val());
+
+  	$("#city").change(function(){
+  		getWeather($("#city").val());
+	});
+
+  
+});
+function getWeather(city){
+  var observe="http://api.yytianqi.com/observe?city="+city+"&key=3loc0opwu29a46ci";
+  //alert(u);
+  $.getJSON(observe,function(json){
+      $("#lastUpdate").html(json.data.lastUpdate);
+      $("#qw").html(json.data.qw);
+      $("#tq").html(json.data.tq);
+      $("#sd").html(json.data.sd);
+      });
+  var air="http://api.yytianqi.com/air?city="+city+"&key=3loc0opwu29a46ci";
+  $.getJSON(air,function(json){
+      $("#pm25").html(json.data.pm25);
+      $("#grade").html(json.data.grade);
+      });
+}
